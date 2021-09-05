735c7dd2b8f4d2890718bebec7fa714975d35397
Artem B <artembritvin@gmail.com>
2021-09-05 22:14:15 +0300

    Реализован контроллер установки и вывода силы тока

--- RohdeSchwarzWebApp/Controllers/HomeController.cs
+++ RohdeSchwarzWebApp/Controllers/HomeController.cs
@@ -2,22 +2,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RohdeSchwarzWebApp.Models;
+using RohdeSchwarzWebApp.Services.Interfaces;
 
 namespace RohdeSchwarzWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAmperageService _AmperageService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IAmperageService AmperageService, ILogger<HomeController> logger)
         {
+            _AmperageService = AmperageService;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() => View();
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult SetAmperage(AmperageViewModel Model)
         {
-            return View(
-                );
+            if(!ModelState.IsValid)
+                return View(nameof(Index), Model);
+
+            _AmperageService.SetAmperage(Model.Amperage);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetAmperage()
+        {
+            return PartialView("Partial/_AmperagePartial", new AmperageViewModel
+            {
+                Amperage = _AmperageService.GetAmperage()
+            });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
