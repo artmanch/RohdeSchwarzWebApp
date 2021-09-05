84cdb26c166402e2c09fe4248aff9a22baeeb241
Artem B <artembritvin@gmail.com>
2021-09-05 18:41:18 +0300

    Создана модель устройства

---
+++ RohdeSchwarzWebApp/Models/AmperateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RohdeSchwarzWebApp.Models
+{
+    public class AmperageViewModel
+    {
+        [Display(Name = "Сила тока")]
+        [Range(0, double.MaxValue, ErrorMessage = "Сила тока не должна быть меньше нуля")]
+        public double Amperage { get; set; }
+    }
+}
