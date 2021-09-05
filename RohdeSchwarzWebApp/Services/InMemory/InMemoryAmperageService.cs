f101a952a755e516239045e22a6718845b7f2899
Artem B <artembritvin@gmail.com>
2021-09-05 19:34:50 +0300

    Реализация-заглушка сервиса работы с силой тока устройств

---
+++ RohdeSchwarzWebApp/Services/InMemory/InMemoryAmperageService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using RohdeSchwarzWebApp.Services.Interfaces;
+
+namespace RohdeSchwarzWebApp.Services.InMemory
+{
+    public class InMemoryAmperageService : IAmperageService
+    {
+        private readonly ILogger<InMemoryAmperageService> _Logger;
+        private double Amperage { get; set; }
+
+        public InMemoryAmperageService(ILogger<InMemoryAmperageService> Logger) => _Logger = Logger;
+
+        public double GetAmperage() => Amperage;
+
+        public bool SetAmperage(double amperage)
+        {
+            if(amperage < 0 || amperage > double.MaxValue)
+            {
+                _Logger.LogError($"Введено значение {amperage} вне диапозона силы тока");
+                return false;
+            }
+
+            Amperage = amperage;
+            _Logger.LogInformation($"Установлено новое значение силы тока устройства {amperage}");
+
+            return true;
+        }
+    }
+}
