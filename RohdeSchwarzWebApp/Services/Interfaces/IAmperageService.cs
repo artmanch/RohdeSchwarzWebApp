9ca59a0c4dfb1c5bc4d52357fe27264a217a04ba
Artem B <artembritvin@gmail.com>
2021-09-05 19:34:26 +0300

    Создан сервис работы с силой тока устройств

---
+++ RohdeSchwarzWebApp/Services/Interfaces/IAmperageService.cs
@@ -0,0 +1,17 @@
+﻿namespace RohdeSchwarzWebApp.Services.Interfaces
+{
+    public interface IAmperageService
+    {
+        /// <summary>
+        /// Получаем силу тока из источника
+        /// </summary>
+        /// <returns>сила тока</returns>
+        public double GetAmperage();
+
+        /// <summary>
+        /// Установить силу тока источника
+        /// </summary>
+        /// <returns>сила тока</returns>
+        public bool SetAmperage(double amperage);
+    }
+}
